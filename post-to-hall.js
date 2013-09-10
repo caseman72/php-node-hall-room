12c6326894bf4315c60d123d06439b5388ad7aee
Casey Manion <CaseyM@marketleader.com>
2013-09-10 11:52:47 -0700

    adding the ability to pass in user and pwd

--- post-to-hall.js
+++ post-to-hall.js
@@ -11,18 +11,26 @@
 	};
 
 	var app = process.argv[1];
-	if (process.argv.length !== 4) {
+	if (process.argv.length < 4) {
 		console.log("Usage: node %s room_id file_path".replace(/%s/, app));
-		return;
+		process.exit(1);
 	}
 
+	// required values
 	var room_id = process.argv[2];
-
 	var file = process.argv[3];
+
+	// pass in user + password
+	if (process.argv.length == 6) {
+		hall_config.email = process.argv[4];
+		hall_config.password = process.argv[5];
+	}
+
+	// open file
 	fs.readFile(file, {encoding: "utf8"}, function (err, data) {
 		if (err) {
 			console.log("Usage: node %s room_id file_path".replace(/%s/, app));
-			return;
+			process.exit(1);
 		}
 
 		var standup = (""+data).replace(/^#-.*$\n/mg, "").replace(/^(\s|\u00A0)+|(\s|\u00A0)+$/g, "");
